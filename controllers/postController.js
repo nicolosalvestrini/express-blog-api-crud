4b6a8f0a17748fc784accda79bcac24151aec1f2
nicolosalvestrini <nicolo.salvestrini@gmail.com>
2026-04-19 23:42:33 +0200

    fixed id recovery

--- controllers/postController.js
+++ controllers/postController.js
@@ -12,7 +12,7 @@ function index(req, res) {
 }
 
 function show(req, res) {
-  const id = req.params.id;
+  const id = parseInt(req.params.id);
   const post = posts.find((post) => post.id === id);
   if (!post) {
     res.status(404).json({
@@ -37,7 +37,7 @@ function store(req, res) {
 }
 
 function update(req, res) {
-  const id = req.params.id;
+  const id = parseInt(req.params.id);
 
   const post = posts.find((post) => post.id === id);
 
@@ -57,7 +57,7 @@ function update(req, res) {
 }
 
 function patch(req, res) {
-  const id = req.params.id;
+  const id = parseInt(req.params.id);
 
   const post = posts.find((post) => post.id === id);
 
@@ -88,7 +88,7 @@ function patch(req, res) {
 }
 
 function destroy(req, res) {
-  const id = req.params.id;
+  const id = parseInt(req.params.id);
   const index = posts.findIndex((post) => post.id === id);
   if (index === -1) {
     res.status(404).json({
