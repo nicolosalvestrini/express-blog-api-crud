46d6397abbbb3a07cdbe587f127c4b85bb930cd8
nicolosalvestrini <nicolo.salvestrini@gmail.com>
2026-04-19 23:40:03 +0200

    add patch and update function

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,62 +1,104 @@
-const posts = require('./../data/data')
-
+const posts = require("./../data/data");
 
 function index(req, res) {
-    const tag = req.query.tag;
+  const tag = req.query.tag;
 
-    if (tag) {
-        const filteredPost = posts.filter(post => post.tags.includes(tag))
-        return res.json(filteredPost)
-    }
+  if (tag) {
+    const filteredPost = posts.filter((post) => post.tags.includes(tag));
+    return res.json(filteredPost);
+  }
 
-    res.json(posts)
+  res.json(posts);
 }
 
 function show(req, res) {
-    const id = (req.params.id);
-    const post = posts.find(post => post.id === id)
-    if (!post) {
-        res.status(404).json({
-            error : 'non esiste alcun post con questo id'
-        })
-        return
-    }
-    res.json(post)
+  const id = req.params.id;
+  const post = posts.find((post) => post.id === id);
+  if (!post) {
+    res.status(404).json({
+      error: "non esiste alcun post con questo id",
+    });
+    return;
+  }
+  res.json(post);
 }
 
 function store(req, res) {
-    const newpost = {
-        id: (posts.length + 1).toString(),
-        titolo: req.body.titolo,
-        contenuto: req.body.contenuto,
-        immagine: req.body.immagine,
-        tags: req.body.tags,
-    }
-    posts.push(newpost)
+  const newpost = {
+    id: (posts.length + 1).toString(),
+    titolo: req.body.titolo,
+    contenuto: req.body.contenuto,
+    immagine: req.body.immagine,
+    tags: req.body.tags,
+  };
+  posts.push(newpost);
 
-    res.json(posts)
+  res.json(posts);
 }
 
 function update(req, res) {
-    res.send('sostituisci un post')
+  const id = req.params.id;
+
+  const post = posts.find((post) => post.id === id);
+
+  if (!post) {
+    res.status(404).json({
+      error: "non esiste alcun post con questo id",
+    });
+    return;
+  }
+
+  post.titolo = req.body.titolo;
+  post.immagine = req.body.immagine;
+  post.tags = req.body.tags;
+  post.contenuto = req.body.contenuto;
+
+  res.json(post);
 }
 
 function patch(req, res) {
-    res.send('hai modificato un post')
+  const id = req.params.id;
+
+  const post = posts.find((post) => post.id === id);
+
+  if (!post) {
+    res.status(404).json({
+      error: "non esiste alcun post con questo id",
+    });
+    return;
+  }
+
+  if (req.body.titolo !== undefined) {
+    post.titolo = req.body.titolo;
+  }
+
+  if (req.body.contenuto !== undefined) {
+    post.contenuto = req.body.contenuto;
+  }
+
+  if (req.body.immagine !== undefined) {
+    post.immagine = req.body.immagine;
+  }
+
+  if (req.body.tags !== undefined) {
+    post.tags = req.body.tags;
+  }
+
+  res.json(post);
 }
 
 function destroy(req, res) {
-    const id = (req.params.id);
-    const index = posts.findIndex((post) => post.id === id);
-    if (index === -1) {
-        res.status(404).json({
-            error : 'non esiste alcun post con questo id'
-        });
-    }
+  const id = req.params.id;
+  const index = posts.findIndex((post) => post.id === id);
+  if (index === -1) {
+    res.status(404).json({
+      error: "non esiste alcun post con questo id",
+    });
+  }
 
-    posts.splice(index, 1);
+  posts.splice(index, 1);
 
-    res.json(posts);
+  res.json(posts);
 }
 
-module.exports = { index, show, store, update, patch, destroy }
+module.exports = { index, show, store, update, patch, destroy };
